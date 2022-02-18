fdb3c8b7ad17cc91d269cbb87fe039bc76d1003e
Stephen Monaco <stevemonaco@hotmail.com>
2022-02-18 10:17:38 -0500

    Add project files.

---
+++ WpfMenu/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace WpfMenu;
+
+public enum SizeOption { Small, Medium, Large }
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty] ObservableCollection<ToppingViewModel> _toppings;
+    [ObservableProperty] SizeOption _sizeOption;
+
+    public MainViewModel()
+    {
+        _toppings = new ObservableCollection<ToppingViewModel>()
+        {
+            new ToppingViewModel("Tomato", 0.50m),
+            new ToppingViewModel("Salami", 0.50m),
+            new ToppingViewModel("Olives", 0.50m),
+        };
+
+        _sizeOption = SizeOption.Medium;
+    }
+}
---
+++ WpfMenu/ToppingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WpfMenu;
+public partial class ToppingViewModel : ObservableObject
+{
+    [ObservableProperty] string _name;
+    [ObservableProperty] decimal _price;
+    [ObservableProperty] bool _isOrdered;
+
+    public ToppingViewModel(string name, decimal price)
+    {
+        _name = name;
+        _price = price;
+    }
+}
